5fd0ea4a32022dbad2ce9b446e8518fcfa2ed85a
kirankembari232323 <kirankembari22@gmail.com>
2024-02-23 15:37:02 +0530

    cart issue fixes added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,12 +27,12 @@ const Navbar = () => {
           </div>
         </Link>
         <div className="cursor-pointer hover:text-cyan-500 transition duration-300 ease-in">
-          <input
-            className="relative m-0 -mr-0.5 block min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-            value={value}
-            placeholder="Search . . . . ."
-            onChange={(e) => handleOnChange(e)} // Call handleOnChange on input change
-          />
+           <input 
+           className="relative m-0 -mr-0.5 block min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary" 
+           value={value}
+           placeholder="Search . . . . ."
+           onChange={(e) => handleOnchange(e)}
+           />
         </div>
         <div className="flex list-none items-center space-x-6 mr-5 text-gray-700 -tracking-tighterr font-semibold">
           <Link to={"/"}>
