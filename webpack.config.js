9984caf09f50e51d4e8b328648c4f3fe48c048de
shashank-yadav-11 <shashank_yadav@persistent.com>
2024-02-23 22:38:50 +0530

    added bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development', // Set mode to development
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, ''),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   module: {
