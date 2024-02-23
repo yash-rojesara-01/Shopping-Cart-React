d880b6539950481cc3c1303e556bab25b8dac4aa
shashank-yadav-11 <shashank_yadav@persistent.com>
2024-02-23 19:46:42 +0530

    added webpack file

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+// postcss.config.js
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Add other PostCSS plugins as needed
+  ],
+};
