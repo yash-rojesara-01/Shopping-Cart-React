c30f2b2ac97fcfc0467b2af5144591ff297a6843
amolaher92 <amolaher92@gmail.com>
2024-02-24 14:47:14 +0530

    updated detail modal for product

---
+++ src/components/DetailModal.jsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
+export default function DetailModal({ item, open, handleClose }) {
+  // const [open, setOpen] = React.useState(false);
+  const [fullWidth, setFullWidth] = React.useState(false);
+  const [maxWidth, setMaxWidth] = React.useState("sm");
+
+  // const handleClickOpen = () => {
+  //   setOpen(true);
+  // };
+
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
+
+  return (
+    <React.Fragment>
+      {/* <Button variant="outlined">open max-width dialog</Button> */}
+      <Dialog
+        fullWidth={fullWidth}
+        maxWidth={maxWidth}
+        open={open}
+        // onClose={handleClose}
+      >
+        <DialogContent>
+          <DialogContentText>
+            <Box sx={{ width: "100%" }}>
+              <Grid
+                container
+                rowSpacing={1}
+                columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+              >
+                <Grid item xs={4}>
+                  <div className="">
+                    <img src={item.image} className="object-cover p-4" />
+                  </div>
+                </Grid>
+                <Grid item xs={8}>
+                  <div className="p-8 space-y-4">
+                    <div className="font-black text-lg">{item.title}</div>
+                    <div className="text-blue-500">
+                      <p>{item.category}</p>
+                    </div>
+                    <div className="text-2xl font-black text-green-700">
+                      ${Number.parseInt(item.price)}
+                    </div>
+                    <div className="text-gray-700">{item.description}</div>
+                  </div>
+                </Grid>
+              </Grid>
+            </Box>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <div className="border border-red-700 rounded-md bg-red-200">
+            <Button onClick={handleClose}>Close</Button>
+          </div>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,19 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import { add, remove } from "../redux/Slices/cartSlice";
 import { useSelector, useDispatch } from "react-redux";
 import { useSnackbar } from "notistack";
-
+import DetailModal from "./DetailModal";
 const Product = ({ item }) => {
   const { cart } = useSelector((state) => state);
   const dispatch = useDispatch();
   const { enqueueSnackbar } = useSnackbar();
   const [itemCount, setItemCount] = useState(0);
-
+  const [open, setOpen] = React.useState(false);
   const addToCart = () => {
-    setItemCount(itemCount+1)
-   const itemdata = {...item} 
+    setItemCount(itemCount + 1);
+    const itemdata = { ...item };
 
-   itemdata['uid'] = new Date().getTime().toString()
+    itemdata["uid"] = new Date().getTime().toString();
     dispatch(add(itemdata));
     enqueueSnackbar(`Item added to your cart successfully`, {
       variant: "success",
@@ -22,51 +22,66 @@ const Product = ({ item }) => {
   };
 
   const removeFromCart = () => {
-    if(itemCount !== 0){
-      setItemCount(itemCount-1)
-    dispatch(remove(item.id));
-    enqueueSnackbar(`Item removed from your cart!`, {
-      variant: "warning",
-      autoHideDuration: 3000,
-    });
-  }
+    if (itemCount !== 0) {
+      setItemCount(itemCount - 1);
+      dispatch(remove(item.id));
+      enqueueSnackbar(`Item removed from your cart!`, {
+        variant: "warning",
+        autoHideDuration: 3000,
+      });
+    }
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
     <>
-      <div className="group hover:scale-110 transition duration-300 ease-in flex flex-col items-center border-2 border-cyan-400 gap-3 p-4 h-[350px] mt-10 ml-5  rounded-xl">
+      <div className="group hover:scale-110 transition duration-300 ease-in flex flex-col items-center border-2 border-cyan-400 gap-3 p-4 h-[350px] mt-10 ml-5 rounded-xl">
         <div className="h-[180px]">
-          <img
-            src={item.image}
-            alt={item.title}
-            className="h-full w-full object-cover"
+          <DetailModal
+            item={item}
+            key={`modal-${item.id}`}
+            open={open}
+            handleClose={handleClose}
           />
         </div>
+        <img
+          src={item.image}
+          alt={item.title}
+          className="object-contain h-48 w-28"
+          onClick={handleClickOpen}
+        />
         <div>
-          <h1 className="truncate w-40 mt-3 text-gray-700 font-semibold text-lg">
+          {/* <h1 className=" w-40 mt-3 text-gray-700 font-semibold text-lg"></h1> */}
+          <h5 class="truncate w-40 text-xl font-semibold tracking-tight text-gray-90">
             {item.title}
-          </h1>
+          </h5>
         </div>
-        <div> <p className="description">{item.description}</p></div>
+        <div>
+          <p className="description text-left ml-8">
+            {item.description.length < 15
+              ? item.description
+              : item.description.slice(0, 20) + "..."}
+          </p>
+        </div>
         <div className="price-cont">
-        <span className="price">${item.price}</span>
-
+          <span className="price">${item.price}</span>
           {/* {cart.some((p) => p.id === item.id) ? ( */}
           {/* // group-hover:bg-purple-700 group-hover:text-white transition duration-300 ease-in text-purple-700 border-2 border-purple-700 rounded-lg font-semibold p-3 */}
-            <button
-               className="float-right plus"
-               onClick={addToCart}
-            >
-              +
-            </button>
+          <button className="float-right plus" onClick={addToCart}>
+            +
+          </button>
           {/* ) : ( */}
           <label className="float-right">{itemCount}</label>
-            <button
-            className="float-right minus"
-            onClick={removeFromCart}
-            >
-              -
-            </button>
+          <button className="float-right minus" onClick={removeFromCart}>
+            -
+          </button>
           {/* )} */}
         </div>
       </div>
